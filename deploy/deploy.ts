ae72199ab583272cd5ade6a149a904bfe73e5977
cody-hamilton <flqrgpbe809@outlook.com>
2025-12-26 13:58:37 +0800

    Add deploy flow for Cipher contracts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedCipherNFT = await deploy("CipherNFT", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const deployedCipherMarket = await deploy("CipherMarket", {
+    from: deployer,
+    args: [deployedCipherNFT.address],
+    log: true,
+  });
+
+  console.log(`CipherNFT contract: `, deployedCipherNFT.address);
+  console.log(`CipherMarket contract: `, deployedCipherMarket.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_cipher"; // id required to prevent reexecution
+func.tags = ["Cipher"];
